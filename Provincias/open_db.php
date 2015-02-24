318143f46d7ba8d3947398176ee96606c8f5d047
msobocin <msobocin@192.168.120.37>
2015-02-24 16:45:08 +0100

    bd

--- Provincias/open_db.php
+++ Provincias/open_db.php
@@ -1,10 +1,10 @@
 <?php
-$conn = mysql_connect("localhost", "alumne", "keiL2lai");
+$conn = mysql_connect("localhost", "root", "secret");
 if (!$conn) {
     $log->error('Could not connect: ' . mysql_error());
     die('Could not connect: ' . mysql_error());
 }
-mysql_select_db("provincies", $conn) or die('Could not select jbalmes database.');
+mysql_select_db("provincias", $conn) or die('Could not select jbalmes database.');
 mysql_set_charset('utf8',$conn);
 ?>
 
